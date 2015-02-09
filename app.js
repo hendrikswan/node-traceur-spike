05086d7630cc34e986294834ce8b17e47d8d6505
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2015-02-09 10:45:47 +0200

    added a comment to say what the error is when you run it

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ compiler.compileSingleFile('./esnext.js', './transpiled.js', function(err){
 
     console.log("transpiled?!");
 });
+
+
+/*
+Running this produces: 
+
+/source/examples/node-traceur-spike/node_modules/traceur/bin/traceur.js:27224
+        throw errorReporter.errors;
+                           ^
+./esnext.js:14:7: Semi-colon expected,./esnext.js:15:25: Semi-colon expected
+
+*/
